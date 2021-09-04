53c2c3ba727279c75a755986413562a7eddaeca3
Olga Roganova <roganova.0lga@yandex.ru>
2021-09-05 01:21:14 +0300

    base configuration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { FoxButtonModule } from "fox-box-ui";
+import { FoxButtonModule } from '@fo-ox/fox-box-ui';
 
 @NgModule({
     declarations: [
